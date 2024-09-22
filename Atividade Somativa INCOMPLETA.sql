730075ce309089620dee77ed0de498501eb5bde3
HeitorBortolanza <139270432+HeitorBortolanza@users.noreply.github.com>
2024-09-22 02:33:03 -0300

    Update Atividade Somativa INCOMPLETA.sql

--- Atividade Somativa INCOMPLETA.sql
+++ Atividade Somativa INCOMPLETA.sql
@@ -1,18 +1,29 @@
+DROP SCHEMA IF EXISTS `AtividadeSomativa` ;
+CREATE SCHEMA `AtividadeSomativa` ;
+
+DROP TABLE IF EXISTS AtividadeSomativa.ITENS;
+DROP TABLE IF EXISTS AtividadeSomativa.PEDIDOS;
+DROP TABLE IF EXISTS AtividadeSomativa.TELEFONES;
+DROP TABLE IF EXISTS AtividadeSomativa.ENDERECOS;
+DROP TABLE IF EXISTS AtividadeSomativa.CLIENTES;
+DROP TABLE IF EXISTS AtividadeSomativa.ENTREGADORES;
+DROP TABLE IF EXISTS AtividadeSomativa.SANDUICHES;
+
 CREATE TABLE AtividadeSomativa.SANDUICHES(
-	CodSanduiche INT AUTO_INCREMENT PRIMARY KEY,
-	NomeSanduiche VARCHAR(45) NOT NULL UNIQUE,
-	ValorSanduiche DECIMAL(6,2) NOT NULL
+    CodSanduiche INT AUTO_INCREMENT PRIMARY KEY,
+    NomeSanduiche VARCHAR(45) NOT NULL UNIQUE,
+    ValorSanduiche DECIMAL(6,2) NOT NULL
 );
 
 CREATE TABLE AtividadeSomativa.ENTREGADORES(
-	CodEntregador SMALLINT AUTO_INCREMENT PRIMARY KEY,
-	NomeEntregador VARCHAR(80) NOT NULL,
-	TelefoneEntregador CHAR(11) NOT NULL UNIQUE
+    CodEntregador SMALLINT AUTO_INCREMENT PRIMARY KEY,
+    NomeEntregador VARCHAR(80) NOT NULL,
+    TelefoneEntregador CHAR(11) NOT NULL UNIQUE
 );
 
 CREATE TABLE AtividadeSomativa.CLIENTES (
-	CodCliente INT AUTO_INCREMENT PRIMARY KEY,
-	NomeCliente VARCHAR(80) NOT NULL
+    CodCliente INT AUTO_INCREMENT PRIMARY KEY,
+    NomeCliente VARCHAR(80) NOT NULL
 );
 
 -- Como endereços DEFINITIVAMENTE não podem existir sem cllientes, mas clientes podem existir sem endereços,
@@ -21,12 +32,12 @@ CREATE TABLE AtividadeSomativa.CLIENTES (
 -- caso seja extritamente necessário a adição de um endereço durante o cadastro de um cliente, é possível fazer isso a nível de aplicação,
 -- (assim como impedir que o telefone contenha números, por exemplo).
 CREATE TABLE AtividadeSomativa.ENDERECOS(
-	CodEndereco INT AUTO_INCREMENT,
-	Logradouro VARCHAR(80) NOT NULL,
-	Numero INT NOT NULL,
-	Bairro VARCHAR(45) NOT NULL,
-	Municipio VARCHAR(60) NOT NULL,
-	UF CHAR(2) NOT NULL,
+    CodEndereco INT AUTO_INCREMENT,
+    Logradouro VARCHAR(80) NOT NULL,
+    Numero INT NOT NULL,
+    Bairro VARCHAR(45) NOT NULL,
+    Municipio VARCHAR(60) NOT NULL,
+    UF CHAR(2) NOT NULL,
     CEP CHAR(9) NOT NULL,
     Complemento VARCHAR(100),
     CodCliente INT NOT NULL,
@@ -35,33 +46,33 @@ CREATE TABLE AtividadeSomativa.ENDERECOS(
 );
 
 CREATE TABLE AtividadeSomativa.TELEFONES(
-	CodCliente INT NOT NULL,
-	TelefoneCliente CHAR(14) UNIQUE,
-	FOREIGN KEY (CodCliente) REFERENCES AtividadeSomativa.CLIENTES(CodCliente)
+    CodCliente INT NOT NULL,
+    TelefoneCliente CHAR(14) UNIQUE,
+    FOREIGN KEY (CodCliente) REFERENCES AtividadeSomativa.CLIENTES(CodCliente)
 );
 
 CREATE TABLE AtividadeSomativa.PEDIDOS(
-	CodPedido INT AUTO_INCREMENT PRIMARY KEY,
-	DataEmissao TIMESTAMP NOT NULL,
-	CodEntregador SMALLINT NOT NULL,
+    CodPedido INT AUTO_INCREMENT PRIMARY KEY,
+    DataEmissao TIMESTAMP NOT NULL,
+    CodEntregador SMALLINT NOT NULL,
     CodEndereco INT NOT NULL,
     StatusPedido TINYINT NOT NULL,
-	FOREIGN KEY (CodEntregador) REFERENCES AtividadeSomativa.ENTREGADORES(CodEntregador),
+    FOREIGN KEY (CodEntregador) REFERENCES AtividadeSomativa.ENTREGADORES(CodEntregador),
     FOREIGN KEY (CodEndereco) REFERENCES AtividadeSomativa.ENDERECOS(CodEndereco)
 );
 
 CREATE TABLE AtividadeSomativa.ITENS(
-	CodPedido INT NOT NULL,
-	CodSanduiche INT NOT NULL,
-	Quantidade TINYINT NOT NULL,
-	FOREIGN KEY (CodPedido) REFERENCES AtividadeSomativa.PEDIDOS(CodPedido),
+    CodPedido INT NOT NULL,
+    CodSanduiche INT NOT NULL,
+    Quantidade TINYINT NOT NULL,
+    FOREIGN KEY (CodPedido) REFERENCES AtividadeSomativa.PEDIDOS(CodPedido),
     FOREIGN KEY (CodSanduiche) REFERENCES AtividadeSomativa.SANDUICHES(CodSanduiche),
-	primary key (CodPedido, CodSanduiche) -- Isso impede que haja duplicatas de sanduiches em um mesmo pedido
+    primary key (CodPedido, CodSanduiche) -- Isso impede que haja duplicatas de sanduiches em um mesmo pedido
 );
 
 INSERT INTO AtividadeSomativa.SANDUICHES VALUES
-	(1,"CAPRESE", 15.00),
-	(2,"QUEIJO-QUENTE", 7.00),
+    (1,"CAPRESE", 15.00),
+    (2,"QUEIJO-QUENTE", 7.00),
     (3,"MISTO QUENTE", 8.00),
     (4,"X-BURGUER", 9.00),
     (5,"X-BACON", 14.00),
@@ -72,36 +83,36 @@ INSERT INTO AtividadeSomativa.SANDUICHES VALUES
     (10,"BAURU", 8.50);
 
 INSERT INTO AtividadeSomativa.ENTREGADORES VALUES
-	(1,"Gabriel da Silva", "41997651321"),
-	(2,"Juliana Montenegro", "41997652321"),
+    (1,"Gabriel da Silva", "41997651321"),
+    (2,"Juliana Montenegro", "41997652321"),
     (3,"Pedro Rodrigues Lopes", "41997651323"),
     (4,"Carlos Noronha Junior", "41997651421"),
     (5,"André Santos Durat", "41997651325");
 
 INSERT INTO AtividadeSomativa.CLIENTES VALUES
-	(1, "Antônio Oliveira"),
-	(2, "Maria Silva"),
-	(3, "João Pereira"),
-	(4, "Fernanda Rodrigues"),
-	(5, "Carlos Almeida"),
-	(6, "Isabela Souza"),
-	(7, "Lucas Cardoso"),
-	(8, "Gabriela Ferreira"),
-	(9, "Rafael Martins"),
-	(10, "Beatriz Costa"),
-	(11, "Matheus Moreira"),
-	(12, "Letícia Dias"),
-	(13, "Gustavo Oliveira da Costa Silva Cardoso"),
-	(14, "Juliana Gomes"),
-	(15, "Pedro Melo"),
-	(16, "Larissa Barros"),
-	(17, "Vitor Lopes"),
-	(18, "Camila Nunes"),
-	(19, "Eduardo Farias"),
-	(20, "Mariana Oliveira da Costa Silva Cardoso");
+    (1, "Antônio Oliveira"),
+    (2, "Maria Silva"),
+    (3, "João Pereira"),
+    (4, "Fernanda Rodrigues"),
+    (5, "Carlos Almeida"),
+    (6, "Isabela Souza"),
+    (7, "Lucas Cardoso"),
+    (8, "Gabriela Ferreira"),
+    (9, "Rafael Martins"),
+    (10, "Beatriz Costa"),
+    (11, "Matheus Moreira"),
+    (12, "Letícia Dias"),
+    (13, "Gustavo Oliveira da Costa Silva Cardoso"),
+    (14, "Juliana Gomes"),
+    (15, "Pedro Melo"),
+    (16, "Larissa Barros"),
+    (17, "Vitor Lopes"),
+    (18, "Camila Nunes"),
+    (19, "Eduardo Farias"),
+    (20, "Mariana Oliveira da Costa Silva Cardoso");
     
 INSERT INTO AtividadeSomativa.ENDERECOS VALUES
- 	(1, "Rua das Flores", 100, "Centro", "Curitiba", "PR", "80000-000", null, 1),
+    (1, "Rua das Flores", 100, "Centro", "Curitiba", "PR", "80000-000", null, 1),
     (2, "Avenida Batel", 456, "Batel", "Curitiba", "PR", "80420-000","Ao lado da casa verde, Ed. Castellani Apto 303", 2), -- teste: mesmo CEP que o cliente 12
     (3, "Rua Mateus Leme", 2345, "São Lourenço", "Curitiba", "PR", "80530-000", null, 3),
     (4, "Rua XV de Novembro", 789, "Alto da Glória", "Curitiba", "PR", "80045-000","Em frente à borracharia", 4),
@@ -124,7 +135,7 @@ INSERT INTO AtividadeSomativa.ENDERECOS VALUES
     (21, "Rua Pindamanhangaba", 42069, "Cristo Rei", "Curitiba", "PR", "80070-280", null, 19); -- teste: CLIENTE 19 cadastrou mais de um ENDEREÇO
     
 INSERT INTO AtividadeSomativa.TELEFONES VALUES
-	(1, "(41)99912-3456"),
+    (1, "(41)99912-3456"),
     (3, "(41)99823-4567"),
     (3, "(41)99734-5678"),
     (3, "(41)99645-6789"),
@@ -146,26 +157,26 @@ INSERT INTO AtividadeSomativa.TELEFONES VALUES
     (20, "(41)97901-2345");
     
 INSERT INTO AtividadeSomativa.PEDIDOS VALUES
-	(1, '2024-08-01 09:18:31', 4, 12, 2),
-	(2, '2024-08-02 09:26:12', 3, 3, 2),
+    (1, '2024-08-01 09:18:31', 4, 12, 2),
+    (2, '2024-08-02 09:26:12', 3, 3, 2),
     (3, '2024-08-04 09:28:15', 1, 17, 2),
-	(4, '2024-08-07 10:04:10', 4, 10, 2),
-	(5, '2024-08-08 10:08:59', 5, 15, 2),
+    (4, '2024-08-07 10:04:10', 4, 10, 2),
+    (5, '2024-08-08 10:08:59', 5, 15, 2),
     (6, '2024-08-11 16:44:35', 1, 10, 2),
     (7, '2024-08-12 10:27:36', 3, 10, 2),
     (8, '2024-08-13 11:15:43', 2, 3, 2),
-	(9, '2024-08-14 11:27:20', 3, 13, 2),
-	(10, '2024-08-14 11:34:16', 3, 7, 2),
-	(11, '2024-08-15 11:27:06', 1, 1, 2),
+    (9, '2024-08-14 11:27:20', 3, 13, 2),
+    (10, '2024-08-14 11:34:16', 3, 7, 2),
+    (11, '2024-08-15 11:27:06', 1, 1, 2),
     (12, '2024-08-15 11:27:32', 1, 7, 2),
-	(13, '2024-08-16 12:47:35', 5, 16, 2),
+    (13, '2024-08-16 12:47:35', 5, 16, 2),
     (14, '2024-08-17 13:02:04', 4, 13, 2),
-	(15, '2024-08-17 13:05:59', 4, 8, 2),
-	(16, '2024-08-18 09:18:53', 4, 2, 2),
+    (15, '2024-08-17 13:05:59', 4, 8, 2),
+    (16, '2024-08-18 09:18:53', 4, 2, 2),
     (17, '2024-08-20 09:26:42', 3, 13, 2),
-	(18, '2024-08-21 09:26:21', 3, 7, 2),
+    (18, '2024-08-21 09:26:21', 3, 7, 2),
     (19, '2024-08-21 22:00:40', 3, 20, 2),
-	(20, '2024-09-15 09:18:14', 1, 9, 2),
+    (20, '2024-09-15 09:18:14', 1, 9, 2),
     (21, '2024-09-15 09:18:22', 4, 19, 2),
     (22, '2024-09-15 09:18:49', 3, 16, 2),
     (23, '2024-09-15 10:04:38', 2, 13, 2),
@@ -188,7 +199,7 @@ INSERT INTO AtividadeSomativa.PEDIDOS VALUES
     (40, '2024-09-15 22:00:19', 4, 8, 1);
 
 INSERT INTO AtividadeSomativa.ITENS VALUES
-	(1, 7, 2),
+    (1, 7, 2),
     (2, 10, 1),
     (3, 4, 1),
     (4, 6, 2),
@@ -235,3 +246,11 @@ INSERT INTO AtividadeSomativa.ITENS VALUES
     (38, 2, 1),
     (39, 2, 1),
     (40, 2, 1);
+
+SELECT * FROM AtividadeSomativa.SANDUICHES;
+SELECT * FROM AtividadeSomativa.ENTREGADORES;
+SELECT * FROM AtividadeSomativa.CLIENTES;
+SELECT * FROM AtividadeSomativa.ENDERECOS;
+SELECT * FROM AtividadeSomativa.TELEFONES;
+SELECT * FROM AtividadeSomativa.PEDIDOS where StatusPedido = 0;
+SELECT * FROM AtividadeSomativa.ITENS where CodPedido in (select CodPedido from AtividadeSomativa.PEDIDOS where StatusPedido = 0);
